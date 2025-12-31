af1551e47993d763f73d340b807e0e0d4cadb824
James Bordner <jobordner@ucsd.edu>
2025-12-31 14:06:22 -0800

    cleaning; preparing to add ezperf documentation

--- src/Cello/performance_Performance.hpp
+++ src/Cello/performance_Performance.hpp
@@ -8,6 +8,114 @@
 #ifndef PERFORMANCE_PERFORMANCE_HPP
 #define PERFORMANCE_PERFORMANCE_HPP
 
+//----------------------------------------------------------------------
+// ENUMERATION DECLARATIONS
+//----------------------------------------------------------------------
+
+/// @enum     counter_type_enum
+/// @brief    Counter value type
+enum counter_type_enum {
+  counter_type_unknown,
+  counter_type_rel,
+  counter_type_abs,
+  counter_type_papi,
+  counter_type_user,
+  num_counter_type
+};
+
+enum index_enum {
+  perf_index_time,
+  perf_index_bytes,
+  perf_index_bytes_high,
+  perf_index_bytes_highest,
+  perf_index_bytes_available,
+  perf_index_last,
+  num_perf_index = perf_index_last
+};
+
+/// @enum    perf_region
+/// @brief   region ID's for the Simulation performance object
+enum perf_region {
+  perf_unknown,
+  perf_simulation,
+  perf_cycle,
+  perf_initial,
+  perf_adapt,
+  perf_adapt_post,
+  perf_adapt_enter,
+  perf_adapt_enter_post,
+  perf_adapt_end,
+  perf_adapt_end_post,
+  perf_adapt_update,
+  perf_adapt_update_post,
+  perf_adapt_next,
+  perf_adapt_next_post,
+  perf_adapt_called,
+  perf_adapt_called_post,
+  perf_adapt_exit,
+  perf_adapt_exit_post,
+  perf_adapt_delete,
+  perf_adapt_delete_post,
+  perf_adapt_recv_level,
+  perf_adapt_recv_level_post,
+  perf_adapt_recv_child,
+  perf_adapt_recv_child_post,
+  perf_reduce,
+  perf_reduce_stopping,
+  perf_reduce_adapt,
+  perf_reduce_charm,
+  perf_reduce_initialize,
+  perf_reduce_output,
+  perf_reduce_restart,
+  perf_reduce_balance,
+  perf_reduce_method_balance,
+  perf_reduce_method_check,
+  perf_reduce_method_inference,
+  perf_reduce_method_m1_closure,
+  perf_reduce_method_turbulence,
+  perf_reduce_simulation,
+  perf_reduce_solver_bicgstab,
+  perf_reduce_solver_cg,
+  perf_reduce_solver_dd,
+  perf_reduce_solver_mg0,
+  perf_reduce_method_debug,
+  perf_reduce_method_flux_correct,
+  perf_reduce_method_order_hilbert,
+  perf_reduce_method_order_morton,
+  perf_reduce_method_output,
+  perf_refresh,
+  perf_refresh_post,
+  perf_refresh_start,
+  perf_refresh_start_post,
+  perf_refresh_recv,
+  perf_refresh_recv_post,
+  perf_refresh_child,
+  perf_refresh_child_post,
+  perf_refresh_exit,
+  perf_refresh_exit_post,
+#ifdef CONFIG_SMP_MODE
+  perf_smp,
+  perf_smp_field_face,
+  perf_smp_hierarchy,
+  perf_smp_initial_music,
+  perf_smp_initial_value,
+  perf_smp_method_close_files,
+  perf_smp_solver_bcg,
+#endif
+  perf_balance,
+  perf_control,
+  perf_method,
+  perf_solver,
+  perf_output,
+  perf_stopping,
+  perf_block,
+  perf_exit,
+#ifdef CONFIG_USE_GRACKLE
+  perf_grackle,
+#endif
+  num_perf_region
+};
+
 class Config;
 
 class Performance {
